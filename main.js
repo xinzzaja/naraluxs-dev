f7fcc5588b33edbf7b154f9e7e066f5beb99126b
xinzzaja <wannnzstorejb@gmail.com>
2025-05-09 21:47:22 +0700

    naradev

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Diantara Musibah Terbesar", "Adalah Ketika Kamu Jatuh Cinta", "Tetapi Orang Itu Tidak Mencintai mu"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
