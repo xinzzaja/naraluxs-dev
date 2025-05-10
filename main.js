0d93fbf2b1013fac7462c5c18502ed86c4262a1c
xinzzaja <wannnzstorejb@gmail.com>
2025-05-10 14:23:10 +0700

    naradev

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Aku Mencintaimu Tetapi Aku Tidak Ingin Kau Terluka Terlalu Dalam"],
+    strings: ["Aku Sangat Mencintaimu Tetapi Aku Tidak Ingin Kau Terluka Terlalu Dalam"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
