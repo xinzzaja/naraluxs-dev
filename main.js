650204aabde2e346aa7b810394a1c5fb28c7f50c
xinzzaja <wannnzstorejb@gmail.com>
2025-05-10 14:21:06 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Diantara Musibah Terbesar", "Adalah Ketika Kamu Jatuh Cinta", "Tetapi Orang Itu Tidak Mencintai mu"],
+    strings: ["Aku Mencintaimu Tetapi Aku Tidak Ingin Kau Terluka Terlalu Dalam"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
