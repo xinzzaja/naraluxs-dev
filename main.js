44138e39619130d53c70adac997a7d85b2bf61cf
xinzzaja <wannnzstorejb@gmail.com>
2025-05-10 17:12:18 +0700

    naradev

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Akuu Memiliki Wanita Yang Sangat Akuu Banggakan Namanya Cynthia Shindie Lisnawati Namanya Cantik Bukan? Seperti Orangnya"],
+    strings: ["Akuu Memiliki Wanita Yang Sangat Akuu Banggakan, Namanya Cynthia Shindie Lisnawati, Nama Yang Sangat Cantik Bukan?, Seperti Orangnya"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
