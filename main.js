f2c10e154b4faa6700003df87ae48cbdca75b118
xinzzaja <wannnzstorejb@gmail.com>
2025-05-12 14:14:38 +0700

    naradev

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var typed = new Typed(".text", {
     strings: ["Di Saat Engkau Merasa Tidak Punya Siapa Siapa Untuk Bercerita Ingatlah Masih Ada Allah"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 100,
     loop: true
 });
