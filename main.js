f1d5b453ad361d817f21ea40bcfa051ae7e509a5
xinzzaja <wannnzstorejb@gmail.com>
2025-05-13 10:20:45 +0700

    naradev

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Di Saat Engkau Merasa Tidak Punya Siapa Siapa Untuk Bercerita Ingatlah Masih Ada Allah"],
+    strings: ["Di saat tak ada seseorang yang bersedia memberikan pundaknya untuk tempat kamu mencurahkan kesedihan, hanya doalah yang mampu mengurangi kepedihanmu."],
     typeSpeed: 80,
     backSpeed: 80,
     backDelay: 1000,
